3130f44fcc6441c8bd292cd09b04664ba551ea32
Mr Cthulhu <danielparucho@gmail.com>
2022-12-06 16:56:50 -0300

    setup vercel

---
+++ api/keys.py
@@ -0,0 +1,4 @@
+account_sid = 'AC7c85cd42c3cff44b7edaa85b60352e89'
+auth_token = '9bcf0c1f65cbc68699b937b5624ba752'
+twilio_number = '+13466602964'
+target_number = '+5491156432312'
--- keys.py
+++
@@ -1,4 +0,0 @@
-account_sid = 'AC7c85cd42c3cff44b7edaa85b60352e89'
-auth_token = '330c6ac6b2defae84dbbeb5119112ccf'
-twilio_number = '+13466602964'
-target_number = '+5491156432312'
